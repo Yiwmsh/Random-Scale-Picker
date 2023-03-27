bb8e830eff8d7ae8842a9e00023846b2744ca3a2
yiwmsh <lonelevelsands@gmail.com>
2023-03-27 09:52:29 -0600

    ✨ Random Scale Picker

---
+++ RandomScaleGenerator.py
@@ -0,0 +1,125 @@
+import math
+import random
+
+import PySimpleGUI as sg
+
+# Global Variables
+
+NOTES = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G'
+]
+
+ACCIDENTALS = [
+    '#', '♮', 'b'
+]
+
+COMMON_SCALES = [
+    'major',
+    'harmonic minor',
+    'melodic minor',
+    'natural minor',
+    'pentatonic major',
+    'pentatonic minor',
+    'pentatonic blues',
+    'pentatonic neutral'
+]
+
+MODES = [
+    'ionian',
+    'dorian',
+    'phrygian',
+    'lydian',
+    'mixolydian',
+    'aeolian',
+    'locrian',
+]
+
+KEYS = {
+
+}
+
+for key in [*NOTES, *ACCIDENTALS, *COMMON_SCALES, *MODES]:
+    KEYS[key] = True
+
+# Functions
+
+
+def generateScale() -> str:
+    enabledNotes = list(filter(lambda note: KEYS[note], NOTES))
+    enabledScales = list(
+        filter(lambda scale: KEYS[scale], [*COMMON_SCALES, *MODES]))
+    enabledAccidentals = list(filter(lambda acc: KEYS[acc], ACCIDENTALS))
+
+    warnings = []
+
+    if (len(enabledNotes) == 0):
+        warnings.append('at least one note')
+
+    if (len(enabledScales) == 0):
+        warnings.append('at least one scale')
+
+    if (len(warnings) > 0):
+        return 'Please select' + ", and ".join(warnings) + '.'
+
+    accidental = ''
+    if (len(enabledAccidentals) > 0):
+        accidental = random.choice(enabledAccidentals)
+
+    if (accidental == '♮'):
+        accidental = ''
+
+    return random.choice(enabledNotes) + accidental + \
+        ' ' + random.choice(enabledScales)
+
+
+# Components
+
+
+def labeledCheckbox(label: str, key: str, initialState: bool) -> list:
+    return sg.Checkbox(label, initialState, key='-%s-' % key, enable_events=True)
+
+
+def checkBoxFrame(values, title) -> list:
+    layout = [labeledCheckbox('all', title, initialState=True)]
+    for value in values:
+        layout.append(labeledCheckbox(value, value, KEYS[value]))
+    return [sg.Frame(title, [layout])]
+
+# Window
+
+
+window = sg.Window(title="Scale Generator", layout=[
+                   [checkBoxFrame(NOTES, 'Notes'), [checkBoxFrame(ACCIDENTALS, 'Accidentals'), sg.Text('(If none are selected, ♮ will be assumed)')], checkBoxFrame(COMMON_SCALES, 'Common Scales'), checkBoxFrame(MODES, 'Modes'), [sg.Text("Click 'Next Scale' to randomly pick a scale from the options you have selected.", key='-Readout-')], [sg.Button('Next Scale', key='-Generate-')]]])
+
+# Event Loop
+
+while True:
+    event, values = window.read()
+
+    if (event == sg.WIN_CLOSED or event == 'end'):
+        break
+    elif (any('-%s-' % key == event for key in [*NOTES, *ACCIDENTALS, *COMMON_SCALES, *MODES])):
+        key = event.replace('-', '')
+        KEYS[key] = not KEYS[key]
+    elif (event == '-Notes-'):
+        targetValue = window[event].get()
+        for note in NOTES:
+            KEYS[note] = targetValue
+            window['-%s-' % note].update(targetValue)
+    elif (event == '-Common Scales-'):
+        targetValue = window[event].get()
+        for scale in COMMON_SCALES:
+            KEYS[scale] = targetValue
+            window['-%s-' % scale].update(targetValue)
+    elif (event == '-Modes-'):
+        targetValue = window[event].get()
+        for mode in MODES:
+            KEYS[mode] = targetValue
+            window['-%s-' % mode].update(targetValue)
+    elif (event == '-Accidentals-'):
+        targetValue = window[event].get()
+        for acc in ACCIDENTALS:
+            KEYS[acc] = targetValue
+            window['-%s-' % acc].update(targetValue)
+    elif (event == '-Generate-'):
+        window['-Readout-'].update(generateScale())
